7e5440a5214c7359175ca5f57554485d04a849e1
Adrian Simon <asreboredo@gmail.com>
2025-05-24 01:59:49 +0200

    try wake lock

--- src/components/functional/PlayerControls/PlayerControls.tsx
+++ src/components/functional/PlayerControls/PlayerControls.tsx
@@ -3,16 +3,30 @@ import {useMatchData} from "../../../hooks/useMatchData.tsx";
 import PlayerControl from "./PlayerControl/PlayerControl.tsx";
 import ResetButton from "./ResetButton/ResetButton.tsx";
 import {useFullscreen} from "@reactuses/core";
-import {useRef} from "react";
+import {useEffect, useRef} from "react";
 import {Fullscreen} from "../../common/Fullscreen.tsx";
+import {useWakeLock} from "react-screen-wake-lock";
 
 function PlayerControls() {
     const {matchData, updatePlayer} = useMatchData();
     const {player1, player2} = matchData;
     const ref = useRef<HTMLDivElement>(null);
     const [isFullscreen, {toggleFullscreen}] = useFullscreen(ref)
+    const {request, release} = useWakeLock({
+        onError: (error) => {
+            alert(`Wake lock unsuccessful!, ${error.message}`);
+        },
+    })
 
     console.log('fullscreen', isFullscreen)
+
+    useEffect(() => {
+        if (isFullscreen) {
+            request();
+        } else {
+            release();
+        }
+    }, [isFullscreen, release, request])
 
 
     if (!player1 || !player2) {
